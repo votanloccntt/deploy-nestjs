d3464def0b83d0dab1613c653ef2928c2a824865
votanloccntt <votanloccntt@gmail.com>
2024-06-01 17:05:48 +0700

    restore

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Swagger } from './common/configs/swagger.config';
 import { HttpExceptionFilter } from './common/exception-filters/http.exception-filter';
-import { resolve } from 'path';
-import { writeFileSync } from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,18 +9,5 @@ async function bootstrap() {
   app.useGlobalFilters(new HttpExceptionFilter());
   app.enableCors();
   await app.listen(3000);
-  // get the swagger json file (if app is running in development mode)
-  if (process.env.NODE_ENV === 'development') {
-    const pathToSwaggerStaticFolder = resolve(process.cwd(), 'swagger-static');
-
-    // write swagger json file
-    const pathToSwaggerJson = resolve(
-      pathToSwaggerStaticFolder,
-      'swagger.json',
-    );
-    const swaggerJson = JSON.stringify(document, null, 2);
-    writeFileSync(pathToSwaggerJson, swaggerJson);
-    console.log(`Swagger JSON file written to: '/swagger-static/swagger.json'`);
-  }
 }
 bootstrap();
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,25 +1,25 @@
 import { Get, HttpStatus, Post } from '@nestjs/common';
 import { UserService } from './user.service';
-// import { SwaggerMethod } from 'src/common/decorators/swagger-method.decorator';
+import { SwaggerMethod } from 'src/common/decorators/swagger-method.decorator';
 import { TagController } from 'src/common/decorators/tag-controller.decorator';
 
 @TagController('user')
 export class UserController {
   constructor(private userService: UserService) {}
 
-  //   @SwaggerMethod({
-  //     summary: 'Xem danh sách tài khoản (ADMIN)',
-  //     responses: [
-  //       {
-  //         status: HttpStatus.OK,
-  //         description: 'Xem danh sách tài khoản thành công',
-  //       },
-  //       {
-  //         status: HttpStatus.NOT_FOUND,
-  //         description: 'Không tìm thấy users',
-  //       },
-  //     ],
-  //   })
+  @SwaggerMethod({
+    summary: 'Xem danh sách tài khoản (ADMIN)',
+    responses: [
+      {
+        status: HttpStatus.OK,
+        description: 'Xem danh sách tài khoản thành công',
+      },
+      {
+        status: HttpStatus.NOT_FOUND,
+        description: 'Không tìm thấy users',
+      },
+    ],
+  })
   @Get()
   getUsers() {
     return this.userService.getUsers();
