286c570bf2fa4572b8d139eaeb831208810af88a
votanloccntt <votanloccntt@gmail.com>
2024-06-01 16:40:00 +0700

    rao SwaggerMethod

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,19 +7,19 @@ import { TagController } from 'src/common/decorators/tag-controller.decorator';
 export class UserController {
   constructor(private userService: UserService) {}
 
-  @SwaggerMethod({
-    summary: 'Xem danh sách tài khoản (ADMIN)',
-    responses: [
-      {
-        status: HttpStatus.OK,
-        description: 'Xem danh sách tài khoản thành công',
-      },
-      {
-        status: HttpStatus.NOT_FOUND,
-        description: 'Không tìm thấy users',
-      },
-    ],
-  })
+  //   @SwaggerMethod({
+  //     summary: 'Xem danh sách tài khoản (ADMIN)',
+  //     responses: [
+  //       {
+  //         status: HttpStatus.OK,
+  //         description: 'Xem danh sách tài khoản thành công',
+  //       },
+  //       {
+  //         status: HttpStatus.NOT_FOUND,
+  //         description: 'Không tìm thấy users',
+  //       },
+  //     ],
+  //   })
   @Get()
   getUsers() {
     return this.userService.getUsers();
